e0a308725176d1644f0f3640ce453e8d80b0bfe7
Yllcaka <yllcaka1516457@gmail.com>
2020-02-25 22:19:51 +0100

    added second e-mail

--- sendemail/views.py
+++ sendemail/views.py
@@ -99,7 +99,7 @@ def tripi_madh_me_email(request):
             # from_email = form.cleaned_data['E_mail']
             # message = form.cleaned_data['message']
             try:
-                send_mail(subjekti, content, E_mail, ['yllcaka1516457@gmail.com'])
+                send_mail(subjekti, content, E_mail, ['yllcaka1516457@gmail.com',"yc40353@universum-ks.org"])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('../success/')
