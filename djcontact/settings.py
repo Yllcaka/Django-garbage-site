1c0a922b219c63764879bee7e6c12b1a4bbea0ad
Yllcaka <yllcaka1516457@gmail.com>
2020-02-26 09:42:08 +0100

    Added links

--- djcontact/settings.py
+++ djcontact/settings.py
@@ -130,5 +130,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = "mcnalbania@gmail.com"
+EMAIL_HOST_USER = "s"
 EMAIL_HOST_PASSWORD = "yllcaka1516457"
