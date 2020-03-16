a379198002568a39f9094edf608e1c69d9ffea17
Yllcaka <yllcaka1516457@gmail.com>
2020-03-16 15:21:16 +0500

    Fixed some issues with the greeting

--- templates/MCN/static/JavaScript/greeting.js
+++ templates/MCN/static/JavaScript/greeting.js
@@ -1,12 +1,14 @@
 function Greeting(id) {
     let koha = new Date;
     let stuff = "";
-    if (koha.getHours() < 10)
-        stuff = "Mirmengjesi Client i dashur";
-    else if (koha.getHours() < 18 && koha.getHours() > 10)
+    if (koha.getHours() < 18 && koha.getHours() > 10)
         stuff = "Mirdita Client i dashur";
     else if (koha.getHours() > 18 || koha.getHours() < 5)
         stuff = "Mirmbrema Client i dashur";
+    else if (koha.getHours() < 10)
+        stuff = "Mirmengjesi Client i dashur";
+
+
     for (let i = 0; i < id.length; i++)
         id[i].innerHTML = stuff;
 }
